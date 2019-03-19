2bf44b9b9334474cbb317c0825eac23eb7ceddf8
Khalil <kmlika.ext@mcs.fr>
2019-03-19 17:10:02 +0100

    Composant conversion des devises

--- src/component/ConversionComponent.js
+++ src/component/ConversionComponent.js
@@ -8,8 +8,15 @@ import InputNumber from "./InputNumber";
 class ConversionComponent extends React.Component {
 
     state = {
-        devisesOptions: []
+        tauxDeviseList: []
     }
+
+    devisesOptions = [];
+
+    tauxMontantAchatMap = new Map();
+
+    tauxMontantVenteMap = new Map();
+
 
     toDevisesOptions = (result) => {
         return (
@@ -20,22 +27,35 @@ class ConversionComponent extends React.Component {
     }
 
     constructor(props) {
-        console.log("........Coontructore............");
         super(props);
 
+    }
+
+    toTauxMontantAchatMap = (result) => {
+        let tauxMap = new Map();
+        result.forEach(taux => {tauxMap.set(taux.devise.id, taux.montantAchat);});
+        return tauxMap;
+    }
+
+    toTauxMontantVenteMap = (result) => {
+        let tauxMap = new Map();
+        result.forEach(taux => {tauxMap.set(taux.devise.id, taux.montantVente);});
+        return tauxMap;
     }
 
 
     componentDidMount() {
         Api.get("/taux-echanges-devises/current").then(result => {
-            this.setState({devisesOptions: this.toDevisesOptions(result.data)});
+            this.setState({tauxDeviseList: result.data});
         });
     }
 
 
-
-
     render() {
+
+        this.devisesOptions = this.toDevisesOptions(this.state.tauxDeviseList);
+        this.tauxMontantAchatMap = this.toTauxMontantAchatMap(this.state.tauxDeviseList);
+        this.tauxMontantVenteMap = this.toTauxMontantVenteMap(this.state.tauxDeviseList);
 
 
         return (
@@ -45,7 +65,17 @@ class ConversionComponent extends React.Component {
                     <div className="card-header"><strong>Conversion</strong></div>
                     <div className="card-body">
 
-                        <Formik initialValues={{deviseId : '', montant : '' ,montantAchat: '', montantVente: ''}}
+                        <Formik initialValues={{deviseId: '', montant: '', montantAchat: '', montantVente: ''}}
+                                onSubmit={(values, {setSubmitting, setFieldValue}) => {
+                                    console.log("values = ", values);
+                                    const montant = values.montant;
+                                    const deviseId = parseInt(values.deviseId);
+                                    const montantAchat = montant * this.tauxMontantAchatMap.get(deviseId);
+                                    setFieldValue('montantAchat', montantAchat);
+                                    const montantVente = montant * this.tauxMontantVenteMap.get(deviseId);
+                                    setFieldValue('montantVente', montantVente);
+                                    setSubmitting(false);
+                                }}
                                 validationSchema={Yup.object().shape({
                                     deviseId: Yup.number().required('Devise est obligatoire'),
                                     montant: Yup.number().positive('Montant d\'achat doit être > 0').required('Montant est obligatoire')
@@ -64,60 +94,59 @@ class ConversionComponent extends React.Component {
                                     setFieldValue,
                                 } = props;
                                 return (
-                            <form>
-                                <div className="form-row">
-                                    <div className="form-group col">
-                                        <label htmlFor="deviseId">Devise</label>
-                                        <InputSelectBox
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.deviseId}
-                                            error={errors.deviseId}
-                                            touched={touched.deviseId}
-                                            name="deviseId"
-                                            options={this.state.devisesOptions}
-                                        />
-                                    </div>
-                                    <div className="form-group col">
-                                        <label htmlFor="montant">Montant</label>
-                                        <InputNumber
-                                            name="montant"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.montant}
-                                            error={errors.montant}
-                                            touched={touched.montant}/>
-                                    </div>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="form-row">
+                                            <div className="form-group col">
+                                                <label htmlFor="deviseId">Devise</label>
+                                                <InputSelectBox
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.deviseId}
+                                                    error={errors.deviseId}
+                                                    touched={touched.deviseId}
+                                                    name="deviseId"
+                                                    options={this.devisesOptions}
+                                                />
+                                            </div>
+                                            <div className="form-group col">
+                                                <label htmlFor="montant">Montant</label>
+                                                <InputNumber
+                                                    name="montant"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.montant}
+                                                    error={errors.montant}
+                                                    touched={touched.montant}/>
+                                            </div>
 
-                                    <div className="form-group col">
-                                        <label htmlFor="montantAchat">Montant Achat</label>
-                                        <InputNumber
-                                            disabled
-                                            name="montantAchat"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.montantAchat}
-                                            error={errors.montantAchat}
-                                            touched={touched.montantAchat}/>
-                                    </div>
-                                    <div className="form-group col">
-                                        <label htmlFor="montantVente">Montant Vente</label>
-                                        <InputNumber
-                                            disabled
-                                            name="montantVente"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.montantVente}
-                                            error={errors.montantVente}
-                                            touched={touched.montantVente}/>
-                                    </div>
-                                </div>
-                                <a href="#" className="btn btn-primary">Convertir</a>
+                                            <div className="form-group col">
+                                                <label htmlFor="montantAchat">Montant Achat</label>
+                                                <InputNumber
+                                                    disabled={true}
+                                                    name="montantAchat"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.montantAchat}
+                                                    error={errors.montantAchat}
+                                                    touched={touched.montantAchat}/>
+                                            </div>
+                                            <div className="form-group col">
+                                                <label htmlFor="montantVente">Montant Vente</label>
+                                                <InputNumber
+                                                    disabled={true}
+                                                    name="montantVente"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    value={values.montantVente}
+                                                    error={errors.montantVente}
+                                                    touched={touched.montantVente}/>
+                                            </div>
+                                        </div>
+                                        <button type="submit" id="enregistrer" className="btn btn-primary" disabled={isSubmitting}>Convertir</button>
 
-                            </form> );
+                                    </form>);
                             }}
                         </Formik>
-
 
 
                         <footer className="card-text">cours d'echange 1Euro = 1.200 Dinar (Dernière mise à jour :
